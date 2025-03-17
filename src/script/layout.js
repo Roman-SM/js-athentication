d97bf488deb72b6b667279ae366a9c8ffaee0ab6
Рома <roma@MacBook-Air-Roma.local>
2025-03-17 23:03:35 +0200

    Add Signup page

---
+++ src/script/layout.js
@@ -0,0 +1,11 @@
+export const createElement = (tag, className, text) => {
+  const elem = document.createElement(tag)
+
+  if(className) {
+    elem.className = className
+  }
+  if(text) {
+    elem.innerHTML = text
+  }
+  return elem
+}
